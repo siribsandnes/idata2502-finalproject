7c717db90fe8b7ee40bc57f98a53860acda61f18
Siri Sandnes <siri.b.sandnes@gmail.com>
2024-04-30 20:04:12 +0200

    test fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-sirisandnes"
+    bucket = "terraform-state-bucket-sirisandnes-1"
     key = "website/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_state_locks"
